741310a776a72d33f9ed4523ef4d81b81ff079f3
GuillemSP <guillem0405@gmail.com>
2024-03-12 20:44:48 +0100

    Try

--- app/src/main/java/com/guillem/simonsays/SimonButtonView.kt
+++ app/src/main/java/com/guillem/simonsays/SimonButtonView.kt
@@ -15,20 +15,31 @@ class SimonButtonView(context: Context, attrs: AttributeSet?) : View(context, at
     }
 
 
-
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         paint.color = Color.GREEN
-        canvas.drawCircle(800f,700f,200f,paint)
+        canvas.drawCircle(800f, 700f, 200f, paint)
         paint.color = Color.RED
-        canvas.drawCircle(300f,1200f,200f,paint)
+        canvas.drawCircle(300f, 1200f, 200f, paint)
         paint.color = Color.BLUE
-        canvas.drawCircle(300f,700f,200f,paint)
+        canvas.drawCircle(300f, 700f, 200f, paint)
         paint.color = Color.YELLOW
-        canvas.drawCircle(800f,1200f,200f,paint)
+        canvas.drawCircle(800f, 1200f, 200f, paint)
+    }
 
+    interface CircleClickListener {
+        fun onCircleClicked(circleColor: Int)
+    }
 
-
+    class CircleClickListenerImpl : CircleClickListener {
+        override fun onCircleClicked(circleColor: Int) {
+            when (circleColor) {
+                Color.GREEN -> println("Green circle clicked")
+                Color.RED -> println("Red circle clicked")
+                Color.BLUE -> println("Blue circle clicked")
+                Color.YELLOW -> println("Yellow circle clicked")
+                else -> println("Unknown circle clicked")
+            }
+        }
     }
 }
